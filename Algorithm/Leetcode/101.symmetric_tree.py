416642de553560b29271ee8615036070461855ff
Dangdang001 <donglei.yin@gmail.com>
2018-07-09 16:10:23 -0400

    Update 101.symmetric_tree.py

--- Algorithm/Leetcode/101.symmetric_tree.py
+++ Algorithm/Leetcode/101.symmetric_tree.py
@@ -1,23 +1,26 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Mon Oct 16 15:33:07 2017
-
-101. Symmetric Tree
-
-@author: dyin
-"""
-
 # Definition for a binary tree node.
-# class TreeNode(object):
+# class TreeNode:
 #     def __init__(self, x):
 #         self.val = x
 #         self.left = None
 #         self.right = None
 
-class Solution(object):
+class Solution:
     def isSymmetric(self, root):
         """
         :type root: TreeNode
         :rtype: bool
         """
+        def ismirror(l,r):
+            if l==None and r==None:
+                return True
+            elif l==None or r==None:
+                return False
+            elif l.val==r.val:
+                return ismirror(l.left,r.right) and ismirror(l.right,r.left)
+            else:
+                return False
+        
+        
+        return ismirror(root,root)
         
