344576efaf1b2da8689c2a5a494f4d3554864050
Dangdang001 <donglei.yin@gmail.com>
2018-07-09 16:29:46 -0400

    Update 101.symmetric_tree.py

--- Algorithm/Leetcode/101.symmetric_tree.py
+++ Algorithm/Leetcode/101.symmetric_tree.py
@@ -11,6 +11,7 @@ class Solution:
         :type root: TreeNode
         :rtype: bool
         """
+     # Solution 1: receusively
         def ismirror(l,r):
             if l==None and r==None:
                 return True
@@ -23,4 +24,29 @@ class Solution:
         
         
         return ismirror(root,root)
+    # Solution 2: iterative using stack/queue
+        def isSymmetric(self, root):
+        """
+        :type root: TreeNode
+        :rtype: bool
+        """
+        if root==None:
+            return True
+        stack=[]
+        stack.append(root.left)
+        stack.append(root.right)
+        while stack:
+            p1,p2=stack.pop(), stack.pop()
+            if p1==None and p2==None:
+                continue
+            if p1==None or p2==None:
+                return False
+            if p1.val==p2.val:
+                stack.append(p1.left)
+                stack.append(p2.right)
+                stack.append(p1.right)
+                stack.append(p2.left)
+            else:
+                return False
+        return True
         
